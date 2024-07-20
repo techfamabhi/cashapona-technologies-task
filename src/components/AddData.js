0c057dd9d088c46a625eb660aac25cefce1e03fb
abhishekgouda <abhishekgouda882@gmail.com>
2024-07-21 00:25:26 +0530

    version 2.0

--- src/components/AddData.js
+++ src/components/AddData.js
@@ -1,52 +1,57 @@
-
 import React, { useState } from 'react';
 import Sidebar from './Sidebar';
 import TopNav from './TopNav';
 import { Link, useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+
 function AddData() {
     const navigate = useNavigate();
 
-    const [ranches, setRanches] = useState([]);
-    const [name, setName] = useState('');
-    const [description, setDescription] = useState('');
-    const [areaSize, setAreaSize] = useState('');
-    const [contactPerson, setContactPerson] = useState('');
-    const [email, setEmail] = useState('');
-    const [phoneNumber, setPhoneNumber] = useState('');
-    const [city, setCity] = useState('');
-    const [state, setState] = useState('');
-    const [zipCode, setZipCode] = useState('');
-    const [additionalDetails, setAdditionalDetails] = useState('');
+    const [formData, setFormData] = useState({
+        name: '',
+        description: '',
+        areaSize: '',
+        contactPerson: '',
+        email: '',
+        phoneNumber: '',
+        city: '',
+        state: '',
+        zipCode: '',
+        additionalDetails: ''
+    });
 
+    const [ranches, setRanches] = useState([]);
     const [errors, setErrors] = useState({});
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
 
     const validate = () => {
         let tempErrors = {};
-        const regExp = /^[A-Za-z ]+$/;
         const regnumExp = /^[0-9]{10}$/;
         const emailExp = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-        if (!name) tempErrors.name = 'Required';
+        const stringonly= /^[a-zA-Z]*$/;
+        
+        if (!formData.name) tempErrors.name = 'Required';
+        else if (!stringonly.test(formData.name)) tempErrors.name = 'Invalid name Format accept only string';
 
-        if (!description) tempErrors.description = 'Required';
+        if (!formData.description) tempErrors.description = 'Required';
+        if (!formData.areaSize) tempErrors.areaSize = 'Required';
+        if (!formData.contactPerson) tempErrors.contactPerson = 'Required';
+        else if (!stringonly.test(formData.contactPerson)) tempErrors.contactPerson = 'Invalid contactPerson Format accept only string';
+        if (!formData.email) tempErrors.email = 'Required';
+        else if (!emailExp.test(formData.email)) tempErrors.email = 'Invalid Email Format';
+        if (!formData.phoneNumber) tempErrors.phoneNumber = 'Required';
+        else if (!regnumExp.test(formData.phoneNumber)) tempErrors.phoneNumber = 'Must be a 10 digit number';
+        if (!formData.city) tempErrors.city = 'Required';
+        else if (!stringonly.test(formData.city)) tempErrors.city = 'Invalid city Format accept only string';
 
-        if (!areaSize) tempErrors.areaSize = 'Required';
-
-        if (!contactPerson) tempErrors.contactPerson = 'Required';
-
-        if (!email) tempErrors.email = 'Required';
-        else if (!emailExp.test(email)) tempErrors.email = 'Invalid Email Format';
-
-        if (!phoneNumber) tempErrors.phoneNumber = 'Required';
-        else if (!regnumExp.test(phoneNumber)) tempErrors.phoneNumber = 'Must be a 10 digit number';
-
-
-        if (!city) tempErrors.city = 'Required';
-
-        if (!state) tempErrors.state = 'Required';
-
-        if (!zipCode) tempErrors.zipCode = 'Required';
+        if (!formData.state) tempErrors.state = 'Required';
+        if (!formData.zipCode) tempErrors.zipCode = 'Required';
 
         setErrors(tempErrors);
         return Object.keys(tempErrors).length === 0;
@@ -55,46 +60,36 @@ function AddData() {
     const handleAddData = (e) => {
         e.preventDefault();
         if (validate()) {
-            const ranchData = {
-                name,
-                description,
-                areaSize,
-                contactPerson,
-                email,
-                phoneNumber,
-                city,
-                state,
-                zipCode,
-                additionalDetails,
-            };
+            const newRanch = { ...formData };
 
-            setRanches([...ranches, ranchData]);
+            setRanches([...ranches, newRanch]);
 
             // Store data in localStorage
             const storedData = JSON.parse(localStorage.getItem('ranchData')) || [];
-            storedData.push(ranchData);
+            storedData.push(newRanch);
             localStorage.setItem('ranchData', JSON.stringify(storedData));
 
             // Clear form fields after submission
-            setName('');
-            setDescription('');
-            setAreaSize('');
-            setContactPerson('');
-            setEmail('');
-            setPhoneNumber('');
-            setCity('');
-            setState('');
-            setZipCode('');
-            setAdditionalDetails('');
+            setFormData({
+                name: '',
+                description: '',
+                areaSize: '',
+                contactPerson: '',
+                email: '',
+                phoneNumber: '',
+                city: '',
+                state: '',
+                zipCode: '',
+                additionalDetails: ''
+            });
 
-           alert('New Ranch Added Successfully!!!');
+            alert('New Ranch Added Successfully!!!');
             navigate('/');
         }
     };
 
     return (
         <div className="container-fluid">
-            
             <div className="row">
                 <Sidebar />
                 <div className="w-100 vh-100 position-fixed overlay d-none" id="sidebar-overlay" />
@@ -106,42 +101,42 @@ function AddData() {
                             <section className="row card-body">
                                 <div className="form-group col-md-4">
                                     <label>Name</label>
-                                    <input type="text" className="form-control bg-color1" placeholder="Enter Name" value={name} onChange={(e) => setName(e.target.value)} />
+                                    <input type="text" className="form-control bg-color1" placeholder="Enter Name" name="name" value={formData.name} onChange={handleChange} />
                                     {errors.name && <span className="text-danger">{errors.name}</span>}
                                 </div>
                                 <div className="form-group col-md-8">
                                     <label>Description</label>
-                                    <input type="text" className="form-control bg-color1" placeholder="Enter Description" value={description} onChange={(e) => setDescription(e.target.value)} />
+                                    <input type="text" className="form-control bg-color1" placeholder="Enter Description" name="description" value={formData.description} onChange={handleChange} />
                                     {errors.description && <span className="text-danger">{errors.description}</span>}
                                 </div>
                                 <div className="form-group col-md-4">
                                     <label>Area Size</label>
-                                    <input type="text" className="form-control bg-color1" placeholder="Enter Area Size" value={areaSize} onChange={(e) => setAreaSize(e.target.value)} />
+                                    <input type="number" className="form-control bg-color1" placeholder="Enter Area Size" name="areaSize" value={formData.areaSize} onChange={handleChange} />
                                     {errors.areaSize && <span className="text-danger">{errors.areaSize}</span>}
                                 </div>
                                 <div className="form-group col-md-4">
-                                    <label>Contact Person</label>
-                                    <input type="text" className="form-control bg-color1" placeholder="Enter Contact Person" value={contactPerson} onChange={(e) => setContactPerson(e.target.value)} />
+                                    <label>Contact Person Name</label>
+                                    <input type="text" className="form-control bg-color1" placeholder="Enter Contact Person" name="contactPerson" value={formData.contactPerson} onChange={handleChange} />
                                     {errors.contactPerson && <span className="text-danger">{errors.contactPerson}</span>}
                                 </div>
                                 <div className="form-group col-md-4">
                                     <label>Email</label>
-                                    <input type="text" className="form-control bg-color1" placeholder="Enter Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                                    <input type="text" className="form-control bg-color1" placeholder="Enter Email" name="email" value={formData.email} onChange={handleChange} />
                                     {errors.email && <span className="text-danger">{errors.email}</span>}
                                 </div>
                                 <div className="form-group col-md-4">
                                     <label>Phone Number</label>
-                                    <input type="text" className="form-control bg-color1" placeholder="Enter Phone Number" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
+                                    <input type="text" className="form-control bg-color1" placeholder="Enter Phone Number" name="phoneNumber" value={formData.phoneNumber} onChange={handleChange} />
                                     {errors.phoneNumber && <span className="text-danger">{errors.phoneNumber}</span>}
                                 </div>
                                 <div className="form-group col-md-4">
                                     <label>City</label>
-                                    <input type="text" className="form-control bg-color1" placeholder="Enter City" value={city} onChange={(e) => setCity(e.target.value)} />
+                                    <input type="text" className="form-control bg-color1" placeholder="Enter City" name="city" value={formData.city} onChange={handleChange} />
                                     {errors.city && <span className="text-danger">{errors.city}</span>}
                                 </div>
                                 <div className="form-group col-md-4">
                                     <label>State</label>
-                                    <select className="form-control bg-color1" value={state} onChange={(e) => setState(e.target.value)}>
+                                    <select className="form-control bg-color1" name="state" value={formData.state} onChange={handleChange}>
                                         <option>Select State</option>
                                         <option>Andhra Pradesh</option>
                                         <option>Arunachal Pradesh</option>
@@ -154,23 +149,24 @@ function AddData() {
                                         <option>Uttar Pradesh</option>
                                         <option>Arizona</option>
                                         <option>New Jersey</option>
-                                        <option>Motena</option>
+                                        <option>Montana</option>
                                     </select>
                                     {errors.state && <span className="text-danger">{errors.state}</span>}
                                 </div>
                                 <div className="form-group col-md-4">
                                     <label>Zip Code</label>
-                                    <input type="text" className="form-control bg-color1" placeholder="Enter Zip Code" value={zipCode} onChange={(e) => setZipCode(e.target.value)} />
+                                    <input type="number" className="form-control bg-color1" placeholder="Enter Zip Code" name="zipCode" value={formData.zipCode} onChange={handleChange} />
                                     {errors.zipCode && <span className="text-danger">{errors.zipCode}</span>}
                                 </div>
                                 <div className="form-group col-md-8">
                                     <label>Additional Details</label>
-                                    <textarea className="form-control bg-color1" placeholder='Enter Additional Details' value={additionalDetails} onChange={(e) => setAdditionalDetails(e.target.value)} rows="3"></textarea>
+                                    <textarea className="form-control bg-color1" placeholder='Enter Additional Details' name="additionalDetails" value={formData.additionalDetails} onChange={handleChange} rows="3"></textarea>
                                 </div>
                                 <div className="form-group col-md-12 add-ranch-btn">
                                     <button type="submit" className="btn btn-primary">Add Ranch</button>
-                                <Link to="/add-ranch">    <button type="submit" className="btn btn-primary mx-3">Cancel</button>
-                                </Link>
+                                    <Link to="/add-ranch">
+                                        <button type="button" className="btn btn-primary mx-3">Cancel</button>
+                                    </Link>
                                 </div>
                             </section>
                         </form>
--- src/components/DisplayRanch.js
+++ src/components/DisplayRanch.js
@@ -2,21 +2,86 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import TopNav from './TopNav';
+import DataTable from 'react-data-table-component';
 
 function DisplayRanch() {
     const [ranchData, setRanchData] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const [itemsPerPage] = useState(4); // Number of records per page
 
+    const customStyles = {
+        table: {
+            style: {
+                border: '1px solid #ddd'
+            }
+        },
+        headRow: {
+            style: {
+                backgroundColor: 'white',
+                color: 'black',
+                borderBottom: '1px solid #ddd'
+            }
+        },
+        headCells: {
+            style: {
+                fontSize: '16px',
+                fontWeight: '600',
+                textTransform: 'uppercase',
+                borderBottom: '1px solid #ddd'
+            }
+        },
+        cells: {
+            style: {
+                fontSize: '15px',
+                borderBottom: '1px solid #ddd'
+            }
+        }
+    };
+
+    const columns = [
+        {
+            name: 'Ranch Name',
+            selector: row => row.name,
+            sortable: true
+        },
+        {
+            name: 'Contact Name',
+            selector: row => row.contactPerson,
+            sortable: true
+        },
+        {
+            name: 'Contact Number',
+            selector: row => row.phoneNumber
+        },
+        {
+            name: 'Contact Email',
+            selector: row => row.email,
+            sortable: true
+        },
+        {
+            name: 'City',
+            selector: row => row.city,
+            sortable: true
+        },
+        {
+            name: 'State',
+            selector: row => row.state,
+            sortable: true
+        }
+    ];
+
     useEffect(() => {
         const storedData = JSON.parse(localStorage.getItem('ranchData')) || [];
         setRanchData(storedData);
+       // console.log(storedData)
     }, []);
 
     const indexOfLastItem = currentPage * itemsPerPage;
+    console.log('indexOfLastItem',indexOfLastItem); //4
     const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-    const currentItems = ranchData.slice(indexOfFirstItem, indexOfLastItem);
-
+    console.log('indexOfFirstItem',indexOfFirstItem); //0
+    const currentItems = ranchData.slice(indexOfFirstItem, indexOfLastItem);  //4
+console.log('currentItems',currentItems);
     const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
     return (
@@ -27,47 +92,25 @@ function DisplayRanch() {
                     <div className="w-100 vh-100 position-fixed overlay d-none" id="sidebar-overlay" />
                     <div className="col-md-9 col-lg-10 ml-md-auto px-0">
                         <TopNav />
-                        <div class="row d-flex justify-content-between align-items-center button-left-right">
-  <h2 class="heading1"><span className='color1'>Demo</span>Book Dashboard</h2>
-<b>  <Link to="/add-ranch" class="btn button-color">Add Ranch</Link>
-</b>
-</div>
+                        <div className="row d-flex justify-content-between align-items-center button-left-right">
+                            <h2 className="heading1"><span className='color1'>Demo</span>Book Dashboard</h2>
+                            <Link to="/add-ranch" className="btn button-color">Add Ranch</Link>
+                        </div>
                         <main className="container">
                             <section className="row mt-4">
-                                <table className="table table-bordered">
-                                    <thead>
-                                        <tr>
-                                            <th scope="col">Ranch name</th>
-                                            <th scope="col">Contact Name</th>
-                                            <th scope="col">Contact Number</th>
-                                            <th scope="col">Contact Email</th>
-                                            <th scope="col">City</th>
-                                            <th scope="col">State</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                       
-
-                                        {currentItems.map((ranch, index) => (
-                                            <tr key={index}>
-                                                <td>{ranch.name}</td>
-                                                <td>{ranch.contactPerson}</td>
-                                                <td>{ranch.phoneNumber}</td>
-                                                <td>{ranch.email}</td>
-                                                <td>{ranch.city}</td>
-                                                <td>{ranch.state}</td>
-                                            </tr>
-                                        ))}
-                                    </tbody>
-                                </table>
+                                <DataTable
+                                    columns={columns}
+                                    data={currentItems}
+                                    customStyles={customStyles}
+                                   
+                                />
                             </section>
                             <div className="row mt-4">
                                 <div className="col-sm-6">
-                                 <b>   Showing {indexOfFirstItem + 1} to {Math.min(indexOfLastItem, ranchData.length)} of {ranchData.length} entries
-                                 </b>
+                                    Showing {indexOfFirstItem + 1} to {Math.min(indexOfLastItem, ranchData.length)} of {ranchData.length} entries
                                 </div>
                                 <div className="col-sm-6">
-                                <ul className="pagination justify-content-end">
+                                    <ul className="pagination justify-content-end">
                                         {Array(Math.ceil(ranchData.length / itemsPerPage)).fill().map((_, index) => (
                                             <li key={index} className={`page-item ${currentPage === index + 1 ? 'active' : ''}`}>
                                                 <button onClick={() => paginate(index + 1)} className="page-link">{index + 1}</button>
